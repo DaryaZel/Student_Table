acf66bbb283db358d64723bee1410fe32c58a4d7
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2019-08-06 00:53:41 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: ['./src/index'],
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'js/app.js',
+    publicPath: '/public'
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    overlay: true
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/main.css'
+    })
+
+  ],
+  module: {
+    rules: [
+      {
+
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ],
+
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: '[name].[ext]',
+          outputPath: '/assets/images/'
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'mustache-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env']
+        }
+      },
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: 'cheap-eval-source-map'
+};
